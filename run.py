ae14c61e41d0559ea7e3b5734dfc0eba55c57012
Yzonix <yzonix@gmail.com>
2016-01-07 21:02:54 +0100

    Add more fragments

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ def run_example(test_file, model_file, segments_file):
 	[flagsInd, classesAll, acc] = aS.mtFileClassification(test_file, model_file, "knn", True, segments_file)
 
 run_example("3min_sk_csauth_fast_b73_150616_007_011016.wav", "manxknn", "3min_sk_csauth_fast_b73_150616_007_011016.segments")
-run_example("10min_sk_csauth_fast_b73_150616_007_011016.wav", "manxknn", "10min_sk_csauth_fast_b73_150616_007_011016.segments")
+run_example("10min_sk_csauth_fast_b73_150621_012_201430.wav", "manxknn", "10min_sk_csauth_fast_b73_150621_012_201430.segments")
