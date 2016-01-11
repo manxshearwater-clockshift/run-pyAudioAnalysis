fcf548b26122a78260c6198dc9879aba9eac5fdf
Yzonix <yzonix@gmail.com>
2016-01-11 18:16:26 +0100

    FindAndSaveClasses: init multicore class finder

---
+++ FindAndSaveClasses.py
@@ -0,0 +1,39 @@
+from pyAudioAnalysis import audioSegmentation as aS
+import numpy as np
+import time
+import os
+import multiprocessing
+
+root = "/home/yorick/ManxShearwaterProject/TESTpyAudioAnalysis/dir/4/"
+sound_files = os.listdir(root)
+queue = multiprocessing.JoinableQueue()
+
+def save_out(test_file):
+    [flags_ind, classes_all, acc] = aS.mtFileClassification(test_file, "manxknn", "knn", False)
+    np.save(test_file, flags_ind)
+    return classes_all
+
+class ThreadUrl(multiprocessing.Process):
+  def __init__(self, queue):
+    multiprocessing.Process.__init__(self)
+    self.queue = queue
+
+  def run(self):
+    while True:
+      soundfile = self.queue.get()
+      save_out(root + soundfile)
+      self.queue.task_done()
+
+start = time.time()
+def main():
+  for _ in range(5):
+    t = ThreadUrl(queue)
+    t.start()
+
+  for soundfile in sound_files:
+    queue.put(soundfile)
+
+  queue.join()
+
+main()
+print "Elapsed Time: %s" % (time.time() - start)
