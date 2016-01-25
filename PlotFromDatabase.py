c298f94d06952a736a785776c4c39318491f813a
Yzonix <yzonix@gmail.com>
2016-01-25 16:02:19 +0100

    plotfromdatabase: min max

--- PlotFromDatabase.py
+++ PlotFromDatabase.py
@@ -2,12 +2,14 @@ import DatabaseHelper as dh
 import matplotlib.pyplot as pl
 import numpy as np
 
+
 def extract_shuffles(tuple_list, class_nr):
     count = 0
     for tuple in tuple_list:
         if class_nr == tuple[0]:
             count += 1
     return count
+
 
 def one_day_shuffles_per_hour(bird, day):
     listx = 24 * [0]
@@ -26,8 +28,10 @@ def one_day_shuffles_per_hour(bird, day):
             shuffle_class = 3
         amount_shuffles = extract_shuffles(tuple_list, shuffle_class)
         normalized_shuffles = (float(amount_shuffles) / float(total_events)) * 3600
-        listx[hour] = (normalized_shuffles)
+        listx[hour] = normalized_shuffles
+
     return listx
+
 
 def running_mean(x, N):
     meanList = np.zeros(len(x))
@@ -48,8 +52,10 @@ def running_mean(x, N):
         meanList[y] = np.mean(tempList)
     return meanList
 
+
 def plot_one_day(listx, day, color):
     pl.plot(list(range(0, len(listx))), listx, color, label="Day " + str(day))
+
 
 def avlist(list_):
     avlist =[]
@@ -59,6 +65,25 @@ def avlist(list_):
         avlist.append(sum(elements)/ len(elements))
 
     return avlist
+
+
+def minlist(list_):
+    minslist =[]
+    minlength = min(len(x) for x in list_)
+    for i in range(minlength):
+        elements = [elem[i] for elem in list_]
+        minslist.append(min(elements))
+    return minslist
+
+
+def maxlistf(list_):
+    maxslist =[]
+    minlength = min(len(x) for x in list_)
+    for i in range(minlength):
+        elements = [elem[i] for elem in list_]
+        maxslist.append(max(elements))
+    return maxslist
+
 
 def plot_average_day(listx, listy, bird):
     listxx = avlist(listx)
@@ -95,6 +120,7 @@ def plot_all_days(bird, start_day, stop_day):
     pl.title(bird)
     pl.show()
 
+
 def plot_compare(bird):
     total_list =[]
     total_list1 =[]
@@ -110,6 +136,7 @@ def plot_compare(bird):
         total_list1.append(running_mean_list1)
 
     plot_average_day(total_list,total_list1, bird )
+
 
 def compare_seperateshifted(bird):
     total_list =[]
@@ -135,6 +162,40 @@ def compare_seperateshifted(bird):
     pl.title(bird)
 
     pl.show()
+
+
+def compare_nonshifted_minmax(bird):
+    total_list = []
+    for day in range(16, 22):
+        no_running_mean_list = one_day_shuffles_per_hour(bird, day)
+        running_mean_list = running_mean(no_running_mean_list, 2)
+        total_list.append(running_mean_list)
+    avlists = avlist(total_list)
+    pl.plot(list(range(0, len(avlists))), avlists, "black", label="unshifted days")
+
+    minslist = minlist(total_list)
+    pl.plot(list(range(0, len(minslist))), minslist, "blue", label="min")
+
+    maxlist = maxlistf(total_list)
+    pl.plot(list(range(0, len(maxlist))), maxlist, "red", label="max")
+
+    colors = {22: 'orange', 23: 'grey', 24: 'darkmagenta'}
+    for day in range(22, 25):
+        no_running_mean_list = one_day_shuffles_per_hour(bird, day)
+        running_mean_list = running_mean(no_running_mean_list, 2)
+        plot_one_day(running_mean_list, day, colors[day])
+
+    ax = pl.subplot(111)
+    pl.fill_between(list(range(0, len(maxlist))), minslist, maxlist, facecolor='lightgreen', alpha=0.5)
+    box = ax.get_position()
+    ax.set_position([box.x0,box.y0,box.width * 0.8, box.height])
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    pl.xlabel("hours")
+    pl.ylabel("amount of shuffles")
+    pl.title(bird)
+
+    pl.show()
+
 
 def normalize_running_means(shifted_list, unshifted_list):
     standard = sum(unshifted_list)
@@ -162,6 +223,9 @@ if __name__ == '__main__':
 
     if input_string == "compare_seperateshifted":
         compare_seperateshifted(input_bird)
+
+    if input_string == "compare_nonshifted_minmax":
+        compare_nonshifted_minmax(input_bird)
 
     print("Shifts: b73-FAST, b174-FAST, b179-SOFT, DB4-FAST, DB20-FAST, DB30-SLOW")
 
